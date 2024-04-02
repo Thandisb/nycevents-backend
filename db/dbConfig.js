6772e4a4d17e600653e5ab1fa750a6e5b0d24f54
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2024-04-02 14:27:03 -0400

    addedpgpassword

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,13 +1,14 @@
 const pgp = require("pg-promise")();
 require("dotenv").config()
 
-const {PG_HOST, PG_USER, PG_PORT, PG_DATABASE}= process.env
+const {PG_HOST, PG_USER, PG_PORT, PG_DATABASE, PG_PASSWORD}= process.env
 
 const cn = {
     host: PG_HOST,
     user: PG_USER,
     port: PG_PORT,
-    database: PG_DATABASE
+    database: PG_DATABASE,
+    password: PG_PASSWORD
 }
 
 console.log(cn)
